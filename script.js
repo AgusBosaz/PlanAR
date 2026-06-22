c8dfbc215176909a84ab81aa59f46c8897ca4ade
Agustin <agus.bosaz1@gmail.com>
2026-06-22 17:48:28 -0300

    Funcionalidad: Mostrar categorias

--- script.js
+++ script.js
@@ -5,3 +5,21 @@ const mensaje = document.getElementById("mensaje");
 btnReservar.addEventListener("click", function () {
     mensaje.textContent = "✅ Reserva realizada correctamente";
 });
+
+// Funcionalidad 2: Mostrar categorías
+const btnCategorias = document.getElementById("btnCategorias");
+const contenedorCategorias = document.getElementById("categorias");
+
+const categorias = ["Conciertos", "Ferias", "Deportes"];
+
+btnCategorias.addEventListener("click", function () {
+
+    contenedorCategorias.innerHTML = "";
+
+    categorias.forEach(function (categoria) {
+        const elemento = document.createElement("p");
+        elemento.textContent = categoria;
+        contenedorCategorias.appendChild(elemento);
+    });
+
+});
